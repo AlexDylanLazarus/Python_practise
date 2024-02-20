8c0fd7f80efe8011374262ec0d03321e33624e94
AlexDylanLazarus <114670032+AlexDylanLazarus@users.noreply.github.com>
2024-02-20 07:40:40 +0000

    conditionals with if and elif, else

---
+++ conditional.py
@@ -0,0 +1,23 @@
+person1= input('Please give your name')
+height1= int(input('Provide your height in cm'))
+person2= input('Please give your name')
+height2= int(input('Provide your height in cm'))
+
+#"abc" > "def" is false. Lexical order/lexical comparison.
+
+#version1
+# if (int(height1) > int(height2)):
+#   print(f"{person1} is taller than {person2}")
+# else:
+#   print(f"{person2} is taller than {person1}")
+
+#exit() to exit the repl
+
+#version2
+if (height1> height2):
+  print(f"{person1} is taller than {person2}")
+elif(height1==height2):
+  print(f"{person1} and {person2} are the same height")
+else:
+  print(f"{person2} is taller than {person1}")
+
