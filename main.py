42ce6eb9aed01673ea804761f8b23044dd996cf3
AlexDylanLazarus <114670032+AlexDylanLazarus@users.noreply.github.com>
2024-02-20 07:14:28 +0000

    added slicing exercise to own file

--- main.py
+++ main.py
@@ -32,29 +32,3 @@
 # p_part= int(percent_completed) // 10
 # remaining_part= 10 - p_part
 # print(f"[{'='*p_part}{' '*remaining_part}] {percent_completed}%")
-
-quote="I love python"
-# print(quote[0])
-# print(quote[2])
-# strings are immutable but will give you a copy.
-# print(quote.replace('l','x'))
-# print(quote.upper())
-
-# # Slicing operator
-# #print(quote[2:5]) #the end value is not included so you add one 
-# print(quote[2:6])
-# print(quote[7:13]) # OR you can use
-# print(quote[7:]) #this means till the end
-# print(quote[100]) # will give index out of range error
-#print(quote[-6:-1)
-# print(quote[0:6:2]) #tells you to skip by 2
-# print(quote[::3])
-#print(quote[::-1]) #reverse the string
-
-
-# x=10
-# while x>=0:
-#   print(x)
-#   x=x-1
-
-x = None
--- test.py
+++ test.py
@@ -1,1 +1,28 @@
 print("hello, we have started day 2")
+
+
+# quote="I love python"
+# print(quote[0])
+# print(quote[2])
+# strings are immutable but will give you a copy.
+# print(quote.replace('l','x'))
+# print(quote.upper())
+
+# # Slicing operator
+# #print(quote[2:5]) #the end value is not included so you add one 
+# print(quote[2:6])
+# print(quote[7:13]) # OR you can use
+# print(quote[7:]) #this means till the end
+# print(quote[100]) # will give index out of range error
+#print(quote[-6:-1)
+# print(quote[0:6:2]) #tells you to skip by 2
+# print(quote[::3])
+#print(quote[::-1]) #reverse the string
+
+
+# x=10
+# while x>=0:
+#   print(x)
+#   x=x-1
+
+# x = None
