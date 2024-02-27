6ed237e93de545a4dd117ee1094b413eb835c487
AlexDylanLazarus <alexdylanlazarus@gmail.com>
2024-02-27 15:12:54 +0200

    finished assignment

--- day_7_OOP.py
+++ day_7_OOP.py
@@ -94,6 +94,21 @@ print(gemma.deposit(200))
 
 
 class transactions:
-    def __init__():
+    def __init__(self, id, date, type, amount):
+        self.id = id
+        self.date = date
+        self.type = type
+        self.amount = amount
 
-You have to print it 
+trans = {
+    'first=transactions': [transactions(1, '29 Feb', 'withdraw', 2000)],
+    'second': [transactions(2, '1 Mar', 'deposit', 6000)],
+    'third': [transactions(3, '3 Mar', 'deposit', 7000)]
+}
+
+print("# #  id   Date       Type     Amount  ")
+
+for key, value in trans.items():
+    for i, transaction in enumerate(value, start=1):
+        print(f"# {i}. {transaction.id}  {transaction.date}   {transaction.type}       {transaction.amount}")
+
