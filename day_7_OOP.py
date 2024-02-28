2b4d6fa6250b573ff3e1507d8fae28b9b2891c3c
AlexDylanLazarus <alexdylanlazarus@gmail.com>
2024-02-28 10:00:40 +0200

    Bank encapsulation

--- day_7_OOP.py
+++ day_7_OOP.py
@@ -1,6 +1,6 @@
 # Object oriented programming?
 # If we could model our problem as real life object then our solution is done
-#You are trying to model the real world
+# You are trying to model the real world
 
 import pprint
 
@@ -10,42 +10,42 @@ import pprint
 # 3. doors
 # 4. name
 
-        # car
-#Ferrari
-# 1. engine - v8 
+# car
+# Ferrari
+# 1. engine - v8
 # 2. wheels- 4
 # 3. doors  2
 
-#Toyota taz
+# Toyota taz
 # 1. engine - v4
 # 2. wheels - 4
 # 3. doors - 4
 
+
 # Use init so you dont have to repeat
 class Car:
     def __init__(self, name, engine, wheels, doors):
-        self.name=name
-        self.engine=engine
-        self.wheels=wheels
-        self.doors=doors
+        self.name = name
+        self.engine = engine
+        self.wheels = wheels
+        self.doors = doors
 
     def horn(self):
         return "Vroom Vroom"
 
 
-#self-> to the object created
-# ferrari -> object        
-ferrari=Car('Ferrari','v8',4,4)
-toyota=Car('Toyota','v4',4,4)
-bugatti=Car('Bugatti','w16',4,2)
-GTR=Car('GTR','v6',4,2)
+# self-> to the object created
+# ferrari -> object
+ferrari = Car("Ferrari", "v8", 4, 4)
+toyota = Car("Toyota", "v4", 4, 4)
+bugatti = Car("Bugatti", "w16", 4, 2)
+GTR = Car("GTR", "v6", 4, 2)
 
-print(ferrari.name,ferrari.wheels)
-print(toyota.name,toyota.wheels)
-print(bugatti.name,bugatti.wheels)
-print(GTR.name,GTR.wheels)
+print(ferrari.name, ferrari.wheels)
+print(toyota.name, toyota.wheels)
+print(bugatti.name, bugatti.wheels)
+print(GTR.name, GTR.wheels)
 print(ferrari.horn())
-
 
 
 # this is too long thats why we use __init__
@@ -56,41 +56,64 @@ print(ferrari.horn())
 # ferrari.doors=4
 # print(ferrari.name, ferrari.engine, ferrari.wheels, ferrari.doors)
 
-#Bank Account 
+# Bank Account
 # 1. accno
 # 2. name
 # 3. balance
 
 
-
-
+# encapsulation | putting in all together container | giving access
 class Bank:
+    # Class variable
+    interest_rate = 0.02
+
     def __init__(self, accno, name, balance):
-        self.accno=accno
-        self.name=name
-        self.balance=balance
+        # instance variable
+        self.accno = accno
+        self.name = name
+
+        # private varaible. __ makes it private
+        self.__balance = balance
+
     def display_balance(self):
-        return f"Your balance is : R{self.balance:,}"
-    def withdraw(self,num):
-        return self.balance-num
-    def deposit(self,numb):
-        return self.balance+numb
+        return f"R{self.__balance:,}"
 
-gemma= Bank("123","Gemma Porill", 15_000)
-dhara= Bank("124", "Dhara Kara", 50_001)
-caleb= Bank("125", "Caleb Potts", 100_000)
+    def withdraw(self, num):
+        if num < self.__balance:
+            self.__balance -= num
+            return f"Your balance is: {self.display_balance()}"
+        else:
+            return f"you do not have enough in your account to withdraw"
+
+    def deposit(self, numb):
+        if numb > 0:
+            self.__balance += numb
+            return f"Successfully deposited. {self.display_balance()}"
+
+    def apply_interest(self):
+        self.__balance += self.__balance * Bank.interest_rate
 
 
-print(gemma.display_balance())
-print(gemma.withdraw(200))
-print(gemma.deposit(200))
+gemma = Bank("123", "Gemma Porill", 15_000)
+dhara = Bank("124", "Dhara Kara", 50_001)
+caleb = Bank("125", "Caleb Potts", 100_000)
+
+
+print(gemma.accno)
+# print(gemma.__balance) gives an error
+print(gemma.apply_interest())
+
+
+# print(gemma.display_balance())
+# print(gemma.withdraw(200))
+# print(gemma.deposit(200))
 
 
 # Assignment - Transactions Tomorrow
-# #  id   Date       Type     Amount  
+# #  id   Date       Type     Amount
 # 1.  1  29 Feb   withdraw       2000
 # 2.  2  1 Mar    deposit        6000
-# 3.  3  3 Mar    deposit        7000  
+# 3.  3  3 Mar    deposit        7000
 
 
 class transactions:
@@ -100,15 +123,17 @@ class transactions:
         self.type = type
         self.amount = amount
 
+
 trans = {
-    'first=transactions': [transactions(1, '29 Feb', 'withdraw', 2000)],
-    'second': [transactions(2, '1 Mar', 'deposit', 6000)],
-    'third': [transactions(3, '3 Mar', 'deposit', 7000)]
+    "first=transactions": [transactions(1, "29 Feb", "withdraw", 2000)],
+    "second": [transactions(2, "1 Mar", "deposit", 6000)],
+    "third": [transactions(3, "3 Mar", "deposit", 7000)],
 }
 
 print(" #  id   Date       Type     Amount  ")
 
 for key, value in trans.items():
     for i, transaction in enumerate(value, start=1):
-        print(f" {i}. {transaction.id}  {transaction.date}   {transaction.type}       {transaction.amount}")
-
+        print(
+            f" {i}. {transaction.id}  {transaction.date}   {transaction.type}       {transaction.amount}"
+        )
--- day_7_scope.py
+++ day_7_scope.py
@@ -1,7 +1,7 @@
-# ctrl + shift + p -> all commands 
+# ctrl + shift + p -> all commands
 #  ctrl + p -> find file
 
-#Lexical scope
+# Lexical scope
 # def market():
 #     price=100
 
@@ -9,7 +9,6 @@
 #         print("the price is :", price)
 #     get_price()
 # market()
-
 
 
 # def cool():
@@ -20,72 +19,83 @@
 # cool()
 # print("the value x is: ", x) #this gives an error saying x is not defined because it is a local variable
 
-#This is what is known as SCOPE
+# This is what is known as SCOPE
 # It is also known as the lifetime of a variable
 
 
-#CLOSURE
+# CLOSURE
 # Own scope + lexical scope. Many functional language supports this
 
-code_word="Hulk"
+code_word = "Hulk"
+
 
 def space_ship():
-    question="Please provide code word"
+    question = "Please provide code word"
 
     def code_word_check():
-        password="Hulk"
+        password = "Hulk"
         print(question)
 
-        if(password==code_word):
+        if password == code_word:
             print(f"Welcome, {password} the strongest avenger")
         else:
             print("Acces denied to ")
+
     code_word_check()
 
+
 space_ship()
- 
+
 # Output:
 # Please provide code word
 # Welcome, Hulk the strongest avenger
 
 
-#Lexical scope is important for currying
-#example of currying
+# Lexical scope is important for currying
+# example of currying
 def add(x):
     def add1(y):
-        return x+y
+        return x + y
+
     return add1
-print(add(10)(5)) #this returns function
-
-add= lambda x: (lambda y: x+y)
 
 
-# Default value copy by reference 
+print(add(10)(5))  # this returns function
+
+add = lambda x: (lambda y: x + y)
+
+
+# Default value copy by reference
 
 # def fun(nums=[]):
 #     nums.append(10)
 #     print(nums)
-    
-#Singleton -> None. It is one of a kind
-def fun(nums=None): #None is an immutable value. None point is used for same memory address
+
+
+# Singleton -> None. It is one of a kind
+def fun(
+    nums=None,
+):  # None is an immutable value. None point is used for same memory address.  Dont give mutable values as default values
     if nums is None:
         nums = []
     nums.append(10)
     print(nums)
 
-fun()    
+
+fun()
 fun()
 fun()
 fun()
 fun([70])
 
-x1=[2,3]
-x2=[2,3]
-x3=x1
-print(x1==x2) #checks the vaue
-print(x1 is x2) #returns false coz they are not pointing at the same memory address/ checks memory address
-print(x1 is x3) #returns true
-
+x1 = [2, 3]
+x2 = [2, 3]
+x3 = x1
+print(x1 == x2)  # checks the value
+print(
+    x1 is x2
+)  # returns false coz they are not pointing at the same memory address/ checks memory address
+print(x1 is x3)  # returns true
 
 
 # fun() #prints [10]
