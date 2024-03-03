928e2e518eefd14b7ef95c006d01e804cb829514
AlexDylanLazarus <alexdylanlazarus@gmail.com>
2024-03-03 09:46:39 +0200

    question 7

--- pyth_fun.py
+++ pyth_fun.py
@@ -120,3 +120,10 @@ for sale in sales:
     else:
         total_sales[salesperson] = amount
 print(total_sales)
+
+# Q7
+# Setup Code
+spells = [("Lumos", 5), ("Obliviate", 10), ("Expelliarmus", 7)]
+# Expected Task: Sort the spells list by power level in descending order using a lambda function.
+sorted_spells = sorted(spells, key=lambda x: x[1], reverse=True)
+print(sorted_spells)
